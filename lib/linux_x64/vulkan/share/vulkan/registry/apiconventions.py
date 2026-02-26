ba1ae5a4a7e7ce557174434a2092d05ab62b9348
KayoRodrigues1235 <kayorodrigues1235@gmail.com>
2026-02-26 20:25:06 -0300

    lib/linux

--- lib/linux_x64/vulkan/share/vulkan/registry/apiconventions.py
+++ lib/linux_x64/vulkan/share/vulkan/registry/apiconventions.py
@@ -1,21 +1,3 @@
-#!/usr/bin/env python3 -i
-#
-# Copyright 2021-2024 The Khronos Group Inc.
-# SPDX-License-Identifier: Apache-2.0
-
-# Generic alias for working group-specific API conventions interface.
-
-# This import should be changed at the repository / working group level to
-# specify the correct API's conventions.
-
-
-import os
-
-defaultAPI = 'vulkan'
-
-VulkanAPI = os.getenv('VULKAN_API', default=defaultAPI)
-
-if VulkanAPI == 'vulkansc':
-    from vkconventions import VulkanSCConventions as APIConventions
-else:
-    from vkconventions import VulkanConventions as APIConventions
+version https://git-lfs.github.com/spec/v1
+oid sha256:e3c61c68c2c2f876fba45de2431a58ed4f2f1e5984527d7629543c4c8091a32f
+size 560
